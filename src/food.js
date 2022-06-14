e70c8bf9dfa4b2075e8d6f67c3288a87f599bd95
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-06-14 11:14:35 -0600

    keyframe

--- src/food.js
+++ src/food.js
@@ -3,9 +3,9 @@ import React from 'react';
 export default function Meal({ food, drink, side }) {
   return (
     <div className='meal'>
-      <img src={`images/${food}.png`} style={{ height:'150' }}/>
-      <img src={`images/${drink}.png`} style={{ height:'150' }}/>
-      <img src={`images/${side}.png`} style={{ height:'150' }} />
+      <img src={`images/${food}.png`} style={{ height:'100' }}/>
+      <img src={`images/${drink}.png`} style={{ height:'100' }}/>
+      <img src={`images/${side}.png`} style={{ height:'100' }} />
     </div>
   );
 }
